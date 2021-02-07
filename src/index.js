89776bd3b7fac40952cb42a768e9a6c8358556ab
UgarovVlad <vlad.ygarov@gmail.com>
2021-02-07 13:47:06 +0300

    ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    // your solution
+
+    str = str.split("");
+    var result = [];
+
+    for (var i = 0; i < str.length; i++) {
+        for(var b = 0; b < bracketsConfig.length; b++) {
+            if(str[i] === bracketsConfig[b][0]) {
+                result.push(bracketsConfig[b][0]);
+                if(str[i] === bracketsConfig[b][1] &&
+                    str[i] === result[result.length-2]) {
+                    result.pop();
+                    result.pop();
+                }
+            }
+            else if (str[i] == bracketsConfig[b][1]) {
+                if(result[result.length-1] != bracketsConfig[b][0]) return false;
+
+                result.pop();
+            }
+        }
+    }
+
+    return result.length === 0;
+};
